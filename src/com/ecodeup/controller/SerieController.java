b5e5e114a9500db7b3e19c68876a789fcc982668
javaee <javaee@javaee-VirtualBox>
2020-06-11 13:29:13 +0200

    ultimos cambios en DAO para listar las series

--- src/com/ecodeup/controller/SerieController.java
+++ src/com/ecodeup/controller/SerieController.java
@@ -1,10 +1,7 @@
 package com.ecodeup.controller;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ecodeup.dao.SerieDAO;
-import com.ecodeup.model.Serie;
 
 /**
  * Servlet implementation class SerieController
@@ -35,114 +31,26 @@ public class SerieController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		SerieDAO serieDAO = new SerieDAO();
 
-		String opcion = request.getParameter("opcion");
+		ArrayList arraySeries = new ArrayList();
 
-		if (opcion.equals("crear")) {
-			System.out.println("Usted a presionado la opcion crear");
-			RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/crear.jsp");
-			requestDispatcher.forward(request, response);
-		} else if (opcion.equals("listar")) {
+		arraySeries = serieDAO.getAll(); // casrteamos la lista a un tipo ArrayList
 
-			SerieDAO serieDAO = new SerieDAO();
-			List<Serie> lista = new ArrayList<>();
-			try {
-				lista = serieDAO.obtenerSeries();
-				for (Serie serie : lista) {
-					System.out.println(serie);
-				}
+		request.setAttribute("series", arraySeries);
 
-				request.setAttribute("lista", lista);
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/listar.jsp");
-				requestDispatcher.forward(request, response);
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
+		requestDispatcher.forward(request, response);
 
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			System.out.println("Usted a presionado la opcion listar");
-		} else if (opcion.equals("meditar")) {
-			int id = Integer.parseInt(request.getParameter("id"));
-			System.out.println("Editar id: " + id);
-			SerieDAO serieDAO = new SerieDAO();
-			Serie p = new Serie();
-			try {
-				p = serieDAO.obtenerSerie(id);
-				System.out.println(p);
-				request.setAttribute("serie", p);
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/editar.jsp");
-				requestDispatcher.forward(request, response);
-
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		} else if (opcion.equals("eliminar")) {
-			SerieDAO serieDAO = new SerieDAO();
-			int id = Integer.parseInt(request.getParameter("id"));
-			try {
-				serieDAO.eliminar(id);
-				System.out.println("Registro eliminado satisfactoriamente...");
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
-				requestDispatcher.forward(request, response);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		}
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String opcion = request.getParameter("opcion");
-		Date fechaActual = new Date();
 
-		if (opcion.equals("guardar")) {
-			SerieDAO serieDAO = new SerieDAO();
-			Serie serie = new Serie();
-			serie.setNombre(request.getParameter("nombre"));
-			serie.setCantidad(Double.parseDouble(request.getParameter("cantidad")));
-			serie.setPrecio(Double.parseDouble(request.getParameter("precio")));
-			serie.setFechaCrear(new java.sql.Date(fechaActual.getTime()));
-			try {
-				serieDAO.guardar(serie);
-				System.out.println("Registro guardado satisfactoriamente...");
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
-				requestDispatcher.forward(request, response);
+		doGet(request, response);
 
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else if (opcion.equals("editar")) {
-			Serie serie = new Serie();
-			SerieDAO serieDAO = new SerieDAO();
-
-			serie.setId(Integer.parseInt(request.getParameter("id")));
-			serie.setNombre(request.getParameter("nombre"));
-			serie.setCantidad(Double.parseDouble(request.getParameter("cantidad")));
-			serie.setPrecio(Double.parseDouble(request.getParameter("precio")));
-			serie.setFechaActualizar(new java.sql.Date(fechaActual.getTime()));
-			try {
-				serieDAO.editar(serie);
-				System.out.println("Registro editado satisfactoriamente...");
-				RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
-				requestDispatcher.forward(request, response);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		// doGet(request, response);
 	}
 
 }
--- src/com/ecodeup/dao/SerieDAO.java
+++ src/com/ecodeup/dao/SerieDAO.java
@@ -3,168 +3,45 @@ package com.ecodeup.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
-import com.ecodeup.conexion.Conexion;
+import com.ecodeup.conexion.ConnectionManager;
 import com.ecodeup.model.Serie;
 
 public class SerieDAO {
-	private Connection connection;
-	private PreparedStatement statement;
-	private boolean estadoOperacion;
 
-	// guardar serie
-	public boolean guardar(Serie serie) throws SQLException {
-		String sql = null;
-		estadoOperacion = false;
-		connection = obtenerConexion();
+	public ArrayList<Serie> getAll() {
 
-		try {
-			connection.setAutoCommit(false);
-			sql = "INSERT INTO series (id, nombre, cantidad, precio, fecha_crear,fecha_actualizar) VALUES(?,?,?,?,?,?)";
-			statement = connection.prepareStatement(sql);
+		ArrayList<Serie> registros = new ArrayList<Serie>();
 
-			statement.setString(1, null);
-			statement.setString(2, serie.getNombre());
-			statement.setDouble(3, serie.getCantidad());
-			statement.setDouble(4, serie.getPrecio());
-			statement.setDate(5, serie.getFechaCrear());
-			statement.setDate(6, serie.getFechaActualizar());
+		String sql = "SELECT id, nombre FROM serie ORDER BY id DESC; ";
 
-			estadoOperacion = statement.executeUpdate() > 0;
+		try (Connection con = ConnectionManager.getConnection(); PreparedStatement pst = con.prepareStatement(sql); ResultSet rs = pst.executeQuery();
 
-			connection.commit();
-			statement.close();
-			connection.close();
-		} catch (SQLException e) {
-			connection.rollback();
-			e.printStackTrace();
-		}
+		) {
 
-		return estadoOperacion;
-	}
+			while (rs.next()) {
 
-	// editar serie
-	public boolean editar(Serie serie) throws SQLException {
-		String sql = null;
-		estadoOperacion = false;
-		connection = obtenerConexion();
-		try {
-			connection.setAutoCommit(false);
-			sql = "UPDATE series SET nombre=?, cantidad=?, precio=?, fecha_actualizar=? WHERE id=?";
-			statement = connection.prepareStatement(sql);
+				// recuperar columnas del resultset
+				int id = rs.getInt("id");
+				String nombre = rs.getString("nombre");
 
-			statement.setString(1, serie.getNombre());
-			statement.setDouble(2, serie.getCantidad());
-			statement.setDouble(3, serie.getPrecio());
-			statement.setDate(4, serie.getFechaActualizar());
-			statement.setInt(5, serie.getId());
+				// crear pojo con datos del rs
+				Serie serie = new Serie();
+				serie.setId(id);
+				serie.setNombre(nombre);
 
-			estadoOperacion = statement.executeUpdate() > 0;
-			connection.commit();
-			statement.close();
-			connection.close();
+				// guardar en ArrayList
+				registros.add(serie);
 
-		} catch (SQLException e) {
-			connection.rollback();
-			e.printStackTrace();
-		}
-
-		return estadoOperacion;
-	}
-
-	// eliminar serie
-	public boolean eliminar(int idSerie) throws SQLException {
-		String sql = null;
-		estadoOperacion = false;
-		connection = obtenerConexion();
-		try {
-			connection.setAutoCommit(false);
-			sql = "DELETE FROM series WHERE id=?";
-			statement = connection.prepareStatement(sql);
-			statement.setInt(1, idSerie);
-
-			estadoOperacion = statement.executeUpdate() > 0;
-			connection.commit();
-			statement.close();
-			connection.close();
-
-		} catch (SQLException e) {
-			connection.rollback();
-			e.printStackTrace();
-		}
-
-		return estadoOperacion;
-	}
-
-	// obtener lista de series
-	public List<Serie> obtenerSeries() throws SQLException {
-		ResultSet resultSet = null;
-		List<Serie> listaSeries = new ArrayList<>();
-
-		String sql = null;
-		estadoOperacion = false;
-		connection = obtenerConexion();
-
-		try {
-			sql = "SELECT * FROM series";
-			statement = connection.prepareStatement(sql);
-			resultSet = statement.executeQuery(sql);
-			while (resultSet.next()) {
-				Serie p = new Serie();
-				p.setId(resultSet.getInt(1));
-				p.setNombre(resultSet.getString(2));
-				p.setCantidad(resultSet.getDouble(3));
-				p.setPrecio(resultSet.getDouble(4));
-				p.setFechaCrear(resultSet.getDate(5));
-				p.setFechaActualizar(resultSet.getDate(6));
-				listaSeries.add(p);
 			}
 
-		} catch (SQLException e) {
+		} catch (Exception e) {
+
 			e.printStackTrace();
 		}
 
-		return listaSeries;
-	}
-
-	// obtener serie
-	public Serie obtenerSerie(int idSerie) throws SQLException {
-		ResultSet resultSet = null;
-		Serie p = new Serie();
-
-		String sql = null;
-		estadoOperacion = false;
-		connection = obtenerConexion();
-
-		try {
-			sql = "SELECT * FROM series WHERE id =?";
-			statement = connection.prepareStatement(sql);
-			statement.setInt(1, idSerie);
-
-			resultSet = statement.executeQuery();
-
-			if (resultSet.next()) {
-				p.setId(resultSet.getInt(1));
-				p.setNombre(resultSet.getString(2));
-				p.setCantidad(resultSet.getDouble(3));
-				p.setPrecio(resultSet.getDouble(4));
-				p.setFechaCrear(resultSet.getDate(5));
-				p.setFechaActualizar(resultSet.getDate(6));
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-		return p;
-	}
-
-	// obtener conexion pool
-	private Connection obtenerConexion() throws SQLException {
-		return Conexion.getConnection();
+		return registros;
 	}
 
 }
